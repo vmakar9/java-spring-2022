b1f99d41e7e0614cff6d16c8839cd0d1fa1c9583
vmakar9 <zmerin212@gmail.com>
2022-12-14 16:16:11 +0200

    1

--- src/main/java/com/example/javaspring2022/models/Customer.java
+++ src/main/java/com/example/javaspring2022/models/Customer.java
@@ -1,2 +1,13 @@
-package com.example.javaspring2022.models;public class Customer {
+package com.example.javaspring2022.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Customer {
+    private int id;
+    private String name;
 }
